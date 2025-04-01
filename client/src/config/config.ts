571c5a9186b0d530c038e9715a2f50e4d61be8ee
Suman Kumar <sumanqaj9876@gmail.com>
2025-04-02 00:49:34 +0530

    d

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_PUBLIC_API_URL || 'http://localhost:5000';
+export const API_URL = import.meta.env.VITE_PUBLIC_API_URL || 'https://quiz-3z2h.onrender.com';
